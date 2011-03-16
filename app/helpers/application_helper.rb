df161b9593126b8fcaf5c6fa11cf89c6639b162e
Drantum <frederik@kerger-mg.de>
2011-03-16 21:02:29 +0100

    Include Gravatar and Syntax Highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-
-  def image_thumbnail(image)
-    return "<a href=\"#{image.thumb_url}\" target=\"_blank\"><img src=\"#{image.thumb_url}\" alt=\"#{image.description}\" title=\"#{image.description}\" border=0></a>"
-  end
   
   def get_errors_for(object) #format errors
    if object.errors
@@ -17,5 +13,23 @@ module ApplicationHelper
  
  def title(page_title)
   content_for(:title) { page_title }
+ end
+
+ def coderay(text)
+   text.gsub!(/\<code(?: lang="(.+?)")?\>(.+?)\<\/code\>/m) do
+     code = CodeRay.scan($2, $1).div(:css => :class)
+     "<notextile>#{code}</notextile>"
+   end
+   return text.html_safe
+ end
+
+ def avatar_url(comment)
+   if File.exists?(RAILS_ROOT + "/public/themes/#{@style}/images/avatar.png")
+	 	default_url = "#{root_url}themes/#{@style}/images/avatar.png"
+	 else
+   	default_url = "#{root_url}images/avatar.png"
+	 end
+   	gravatar_id = Digest::MD5.hexdigest(comment.email.downcase)
+  	"http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
  end
 end
