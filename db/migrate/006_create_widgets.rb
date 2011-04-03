aa1f3a0c3c5953b644eaa8af24461509ba0796f9
Drantum <frederik@kerger-mg.de>
2011-04-03 09:41:09 +0200

    Fix an error in the migration

--- db/migrate/006_create_widgets.rb
+++ db/migrate/006_create_widgets.rb
@@ -7,10 +7,10 @@ class CreateWidgets < ActiveRecord::Migration
       t.column :active, :integer, :default => 1
 			t.column :row, :integer
     end
-   Post.create(:title => "Archive", :content => "menu_archive", :css => "archive", :row => "2")
-   Post.create(:title => "Search", :content => "menu_search", :css => "search", :row => "3")
-   Post.create(:title => "Feeds", :content => "menu_feeds", :css => "feeds", :row => "1")
-   Post.create(:title => "Tags", :content => "menu_tags", :css => "tags", :row => "4")
+   Widget.create(:title => "Archive", :content => "menu_archive", :css => "archive", :row => "2")
+   Widget.create(:title => "Search", :content => "menu_search", :css => "search", :row => "3")
+   Widget.create(:title => "Feeds", :content => "menu_feeds", :css => "feeds", :row => "1")
+   Widget.create(:title => "Tags", :content => "menu_tags", :css => "tags", :row => "4")
   end
 
   def self.down
